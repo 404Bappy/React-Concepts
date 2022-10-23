ff6cb37652056999c0ad5f8f15fd618a31361686
Bappy71 <19103076@iubat.edu>
2022-10-23 06:38:37 +0600

    first commit

--- Component/component-practice/src/App.js
+++ Component/component-practice/src/App.js
@@ -4,27 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
+      <MyComponent brand="Apple" price="156K"></MyComponent>
+      <MyComponent brand="Nokia" price="80k"></MyComponent>
+      <MyComponent brabd="Samsung" price="120k"></MyComponent>
+      <MyComponent brand="Tesla" price="20k"></MyComponent>
     </div>
   );
 }
-function MyComponent() {
+function MyComponent(props) {
+  console.log(props);
   const myStyle = {
     backgroundColor: 'lightblue',
     border: '3px solid blue',
     margin: '20px',
-    padding:'10px',
+    padding: '10px',
     borderRadius: '10px'
 
 
   }
   return (
     <div style={myStyle}>
-      <h1>Hi There This Is Arafat Bappy</h1>
-      <p style={{color:'purple', fontWeight: 'bold'}}>Here is my component</p>
+      <h1>Hi There This Is Arafat Bappy. i like {props.brand}</h1>
+      <h4>Asking Money {props.price}</h4>
+      <p style={{ color: 'purple', fontWeight: 'bold' }}>Here is my component</p>
     </div>
   )
 }
