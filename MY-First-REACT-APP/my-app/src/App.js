56f39ae63ccfc9f076928ab5cb7f1cbd4229ae5e
Bappy71 <19103076@iubat.edu>
2022-11-12 01:29:29 +0600

    initial comit

--- MY-First-REACT-APP/my-app/src/App.js
+++ MY-First-REACT-APP/my-app/src/App.js
@@ -5,16 +5,19 @@ function App() {
   return (
     <div className="App">
       <Product name="Mobile" price="114000"></Product>
-      <Product name="Mobile" price="114000"></Product>
-      <Product name="Mobile" price="114000"></Product>
-      <Product name="Mobile" price="114000"></Product>
-      <Product name="Mobile" price="114000"></Product>
+      <Product name="Laptop" price="214000"></Product>
+      <Product name="watch" price="4000"></Product>
+      <Product name="AirPod" price="114000"></Product>
+      <Product name="Speaker" price="114000"></Product>
     </div>
   );
 }
 function Product(props){
+  const productStyle = {
+    border: '3px solid yellow'
+  }
   return(
-    <div className >
+    <div className="product" style={productStyle}>
       <h2>Name: {props.name}</h2>
       <h4>Price: {props.price}</h4>
     </div>
