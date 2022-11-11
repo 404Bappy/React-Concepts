afe9cc3fbaddc9dd8906b16b5db83fe991efb5ba
Bappy71 <19103076@iubat.edu>
2022-11-12 04:38:42 +0600

    initial comit

--- MY-First-REACT-APP/my-app/src/App.js
+++ MY-First-REACT-APP/my-app/src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const product =[
+    {name: 'Mobile', price:114000},
+    {name: 'Laptop', price:214000},
+    {name: 'watch', price:4000},
+    {name: 'AirPod', price:114000},
+    {name: 'Speaker', price:114000},
+    {name: 'Clock', price:2000},
+
+  ]
   return (
     <div className="App">
-      <Product name="Mobile" price="114000"></Product>
+      {
+        product.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
+
+      {/* <Product name="Mobile" price="114000"></Product>
       <Product name="Laptop" price="214000"></Product>
       <Product name="watch" price="4000"></Product>
       <Product name="AirPod" price="114000"></Product>
-      <Product name="Speaker" price="114000"></Product>
+      <Product name="Speaker" price="114000"></Product> */}
     </div>
   );
 }
