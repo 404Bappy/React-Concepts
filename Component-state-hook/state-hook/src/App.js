72128621f0d8b364d94ffc1651da7013ac0eb3c0
Bappy71 <19103076@iubat.edu>
2022-11-13 23:48:48 +0600

    initial comit

--- Component-state-hook/state-hook/src/App.js
+++ Component-state-hook/state-hook/src/App.js
@@ -2,13 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'Mobile', price: '16000'},
+    { name: 'Speaker', price: '6000'},
+    { name: 'AirBud', price: '8000'},
+    { name: 'DSLR', price: '26000'},
+    { name: 'Monitor', price: '10000'},
+
+  ]
   return (
     <div className="App">
-        <Product name='Mobile' price='16000'></Product>
+        {
+          products.map(products=> <Product name={products.name} price={products.price}></Product> )
+        }
+        
+        
+        {/* <Product name='Mobile' price='16000'></Product>
         <Product name='Speaker' price='6000'></Product>
         <Product name='AirBud' price='8000'></Product>
         <Product name='DSLR' price='26000'></Product>
-        <Product name='Monitor' price='10000'></Product>
+        <Product name='Monitor' price='10000'></Product> */}
     </div>
   );
 }
