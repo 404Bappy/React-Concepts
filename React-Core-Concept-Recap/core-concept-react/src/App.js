82215733eeeedd45465a0a89fb52c5b8c72d918c
Bappy71 <19103076@iubat.edu>
2022-11-20 19:19:06 +0600

    first commit

--- React-Core-Concept-Recap/core-concept-react/src/App.js
+++ React-Core-Concept-Recap/core-concept-react/src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
-      <MyComponent></MyComponent>
+      <MyComponent Brand= 'SuZuki' Price= "265k"></MyComponent>
+      <MyComponent Brand= 'YeamaHA' Price= "365k"></MyComponent>
+      <MyComponent Brand= 'Honda' Price= "465k"></MyComponent>
+      <MyComponent Brand= 'TVS' Price= "165k"></MyComponent>
+      <MyComponent Brand= 'KTM' Price= "565k"></MyComponent>
+      <MyComponent Brand= 'KAWAsAKi' Price= "865k"></MyComponent>
+ 
+     
     </div>
   );
 }
 
-function MyComponent(){
+function MyComponent(props){
+  console.log(props);
   return(
     <div className="myCompo">
-      <h1>Hey There ! This is My Component</h1>
-      <p>I Can Write My Won component</p>
+      <h1>Hey There ! This is : {props.Brand}</h1>
+      <p className="paragraph">Price: {props.Price}</p>
     </div>
   )
 }
