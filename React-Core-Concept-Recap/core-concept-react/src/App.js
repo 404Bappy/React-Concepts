383c5b60f74ddb22872c0e306b31e0735945d5b1
Bappy71 <19103076@iubat.edu>
2022-11-20 18:51:17 +0600

    first commit

--- React-Core-Concept-Recap/core-concept-react/src/App.js
+++ React-Core-Concept-Recap/core-concept-react/src/App.js
@@ -4,22 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
     </div>
   );
+}
+
+function MyComponent(){
+  return(
+    <div className="myCompo">
+      <h1>Hey There ! This is My Component</h1>
+      <p>I Can Write My Won component</p>
+    </div>
+  )
 }
 
 export default App;
