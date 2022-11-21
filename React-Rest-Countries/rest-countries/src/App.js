d643e08d08199b54bbb0824e006e1020eeaf09f8
Bappy71 <19103076@iubat.edu>
2022-11-22 03:33:36 +0600

    first commit

--- React-Rest-Countries/rest-countries/src/App.js
+++ React-Rest-Countries/rest-countries/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-     
+        <Countries></Countries>
     </div>
   );
 }
+//Create Component to load countries//
+function Countries(){
+  const [countries, setCountries] = useState([]);
+  useEffect(() =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+  }, [])
+  return(
+    <div>
+        <h2>Traveling Around The World..!!</h2>
+        <h4>Countries Available : {countries.length}</h4>
+    </div>
+  )
+}
+
 
 export default App;
