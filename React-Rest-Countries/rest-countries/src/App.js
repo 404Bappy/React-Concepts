c56335203370765b8f8ac9538e8f42e5749c90b9
Bappy71 <19103076@iubat.edu>
2022-11-22 03:37:45 +0600

    first commit

--- React-Rest-Countries/rest-countries/src/App.js
+++ React-Rest-Countries/rest-countries/src/App.js
@@ -5,22 +5,22 @@ import { useEffect, useState } from "react";
 function App() {
   return (
     <div className="App">
-        <Countries></Countries>
+      <Countries></Countries>
     </div>
   );
 }
 //Create Component to load countries//
-function Countries(){
+function Countries() {
   const [countries, setCountries] = useState([]);
-  useEffect(() =>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
   }, [])
-  return(
+  return (
     <div>
-        <h2>Traveling Around The World..!!</h2>
-        <h4>Countries Available : {countries.length}</h4>
+      <h2>Traveling Around The World..!!</h2>
+      <h4>Countries Available : {countries.length}</h4>
     </div>
   )
 }
